c26cdd8c7fdc4cb6d27abaa810a1867a57a2a683
Alfredo <alfloz01@ucm.es>
2023-12-27 12:18:03 +0100

    Create types and entities

---
+++ src/entities/beer.ts
@@ -0,0 +1,12 @@
+import { ImgData } from '../types/img.data.js';
+import { User } from './user.js';
+
+export type Beer = {
+  id: string;
+  name: string;
+  brewer: string;
+  style: string;
+  alcohol: string;
+  beerImg: ImgData;
+  author: User;
+};
---
+++ src/entities/user.ts
@@ -0,0 +1,30 @@
+import { Beer } from './beer';
+import joi from 'joi';
+
+export type UserLogin = {
+  password: string;
+  email: string;
+};
+
+export type User = UserLogin & {
+  id: string;
+  name: string;
+  surname: string;
+  age: number;
+  userName: string;
+  probada: Beer[];
+  role: 'Admin' | 'User';
+};
+
+export const userJoiSchema = joi.object<User>({
+  email: joi.string().email().required().messages({
+    'string.base': 'Formato no válido',
+    'string.email': 'Formato de email no válido',
+    'string.required': 'Valor requerido',
+  }),
+  password: joi
+    .string()
+    .pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)
+    .required(),
+  age: joi.number().min(18).max(100),
+});
