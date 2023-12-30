4da6de95136cb34e6584a8c56e3037ca606ff8cf
Alfredo <alfloz01@ucm.es>
2023-12-30 12:02:49 +0100

    Create some test

--- src/repos/user/user.mongo.repo.test.ts
+++ src/repos/user/user.mongo.repo.test.ts
@@ -9,14 +9,16 @@ jest.mock('../beer/beer.mongo.model');
 jest.mock('../pub/pub.mongo.model');
 
 describe('Given UserMongoRepo class', () => {
-  // eslint-disable-next-line no-unused-vars
+  const id = 'validId';
+  const user: User = {} as unknown as User;
   let repo: UserMongoRepo;
-  const exec = jest.fn().mockResolvedValue('name');
+  const exec = jest.fn().mockResolvedValue(user);
+  const hashedPassword = 'hashedPassword';
 
   describe('When we instantiate it without errors', () => {
     beforeEach(() => {
       repo = new UserMongoRepo();
-      UserModel.find = jest.fn().mockReturnValue({
+      UserModel.findOne = jest.fn().mockReturnValue({
         populate: jest.fn().mockReturnValue({
           exec,
         }),
@@ -26,25 +28,32 @@ describe('Given UserMongoRepo class', () => {
           exec,
         }),
       });
-      UserModel.create = jest
+      UserModel.create = jest.fn().mockReturnValue({});
+      UserModel.findByIdAndUpdate = jest
         .fn()
         .mockReturnValue({ populate: jest.fn().mockReturnValue({ exec }) });
-      UserModel.findByIdAndUpdate = jest.fn().mockReturnValue({
-        exec: jest.fn().mockResolvedValue({}),
-      });
       Auth.compare = jest.fn().mockResolvedValue(true);
+      Auth.hash = jest.fn().mockResolvedValue(hashedPassword);
     });
-    const id = 'validId';
-    const user: User = {} as unknown as User;
+    test('should find a user by email and return it when a valid email is provided', async () => {
+      const loginUser = {
+        email: 'test@example.com',
+        password: 'password123',
+      };
+      const result = await repo.login(loginUser);
+      expect(UserModel.findOne).toHaveBeenCalledWith({
+        email: loginUser.email,
+      });
+      expect(result).toEqual(user);
+    });
+    test('should create a new user with valid input', async () => {
+      const result = await repo.create({} as Omit<User, 'id'>);
+      expect(Auth.hash).toHaveBeenCalled();
+      expect(UserModel.create).toHaveBeenCalled();
+      expect(result).toEqual(user);
+    });
     test('should return a user object when a valid id is passed', async () => {
       const repo = new UserMongoRepo();
-      const exec = jest.fn().mockResolvedValue(user);
-      UserModel.findById = jest.fn().mockReturnValue({
-        populate: jest.fn().mockReturnValue({
-          exec,
-        }),
-      });
-
       const result = await repo.getById(id);
       expect(UserModel.findById).toHaveBeenCalledWith(id);
       expect(result).toEqual(user);
