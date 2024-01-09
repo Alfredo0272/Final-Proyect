e456ce4ec7017405cb0b1c6690a5536568ab35c8
Alfredo <alfloz01@ucm.es>
2024-01-09 10:44:04 +0100

    Correcto routes name

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -2,14 +2,18 @@ import { Router as createRouter } from 'express';
 import createDebug from 'debug';
 import { UsersController } from '../controller/user/user.controler';
 import { UserMongoRepo } from '../repos/user/user.mongo.repo';
+import { ValidationInterceptor } from '../middleware/validation.interceptor';
 
 const debug = createDebug('W9Final:user:router');
-
 export const userRouter = createRouter();
-
 debug('Starting');
 const repo = new UserMongoRepo();
 const controller = new UsersController(repo);
+const validationInterceptor = new ValidationInterceptor();
 
 userRouter.post('/login', controller.login.bind(controller));
-userRouter.post('/register', controller.create.bind(controller));
+userRouter.post(
+  '/register',
+  validationInterceptor.registerValidator().bind(validationInterceptor),
+  controller.create.bind(controller)
+);
